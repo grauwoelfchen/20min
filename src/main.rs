7d2b1b2e8569845fa3792e42ec9b649c684b93bb
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-01-31 02:48:33 +0900

    Add args extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io::Write;
+use std::str::FromStr;
+
+
 fn main() {
-    // pass
+    let mut work_t: u64 = 15;
+    let mut rest_t: u64 = 5;
+
+    let mut args = Vec::new();
+    for arg in std::env::args().skip(1) {
+        args.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+
+    if args.len() == 0 {
+        // pass (use default)
+    } else if args.len() == 1 {
+        work_t = args[0];
+    } else if args.len() == 2 {
+        work_t = args[0];
+        rest_t = args[1];
+    } else {
+        writeln!(std::io::stderr(),
+                 "Usage: 20min n (working time) n (time for rest)").unwrap();
+        std::process::exit(1);
+    }
+
+    println!("work_t {}, rest_t {}", work_t, rest_t);
 }
