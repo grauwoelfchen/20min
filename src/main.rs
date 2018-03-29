cde48993c6b796572e8df2169ac0b5d1680c0dfa
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-03-29 23:45:26 +0900

    Apply fmt using `.rustfmt.toml`

--- src/main.rs
+++ src/main.rs
@@ -7,108 +7,116 @@ use ticker::tick;
 mod ticker;
 
 fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
-    match s.find(separator) {
-        None => None,
-        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-            (Ok(l), Ok(r)) => Some((l, r)),
-            _ => None,
-        },
-    }
+  match s.find(separator) {
+    None => None,
+    Some(index) => {
+      match (
+        T::from_str(&s[..index]),
+        T::from_str(&s[index + 1..]),
+      ) {
+        (Ok(l), Ok(r)) => Some((l, r)),
+        _ => None,
+      }
+    },
+  }
 }
 
 /// parse arg as string
 fn split_arg(s: &str) -> Option<Vec<String>> {
-    match parse_pair::<String>(&s, ',') {
-        Some((w, r)) => Some(vec![w, r]),
-        None => None,
-    }
+  match parse_pair::<String>(&s, ',') {
+    Some((w, r)) => Some(vec![w, r]),
+    None => None,
+  }
 }
 
 fn report(duration: u64) {
-    println!(
-        "{prefix:>prefix_width$} {info} target(s) in {duration} secs\r",
-        prefix = "Finished",
-        prefix_width = 12,
-        info = "work [unoptimized + progressbar]",
-        duration = duration
-    );
+  println!(
+    "{prefix:>prefix_width$} {info} target(s) in {duration} secs\r",
+    prefix = "Finished",
+    prefix_width = 12,
+    info = "work [unoptimized + progressbar]",
+    duration = duration
+  );
 }
 
 fn main() {
-    let mut args: Vec<String> = std::env::args().skip(1).collect();
-    if args.len() == 1 {
-        // handle single string input like: 15,5
-        if let Some(v) = split_arg(&args[0]) {
-            args = v;
-        }
+  let mut args: Vec<String> = std::env::args().skip(1).collect();
+  if args.len() == 1 {
+    // handle single string input like: 15,5
+    if let Some(v) = split_arg(&args[0]) {
+      args = v;
     }
-    let c = Config::new(args);
+  }
+  let c = Config::new(args);
 
-    if c.params.len() > 2 {
-        writeln!(
-            std::io::stderr(),
-            "Usage: 20min n (working time) n (time for rest)"
-        ).unwrap();
-        std::process::exit(1);
-    }
+  if c.params.len() > 2 {
+    writeln!(
+      std::io::stderr(),
+      "Usage: 20min n (working time) n (time for rest)"
+    ).unwrap();
+    std::process::exit(1);
+  }
 
-    let (work_t, rest_t) = c.to_tuple();
+  let (work_t, rest_t) = c.to_tuple();
 
-    tick(work_t, "Working");
-    report(work_t);
+  tick(work_t, "Working");
+  report(work_t);
 
-    tick(rest_t, "Resting");
-    report(rest_t);
+  tick(rest_t, "Resting");
+  report(rest_t);
 }
 
 #[cfg(test)]
 mod main_test {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_parse_pair() {
-        assert_eq!(parse_pair::<i32>("", ','), None);
-        assert_eq!(parse_pair::<i32>("10,", ','), None);
-        assert_eq!(parse_pair::<i32>(",10", ','), None);
-        assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
-        assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
-        assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
-        assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
-        assert_eq!(
-            parse_pair::<String>("0,5", ','),
-            Some(("0".to_string(), "5".to_string()))
-        );
-        assert_eq!(
-            parse_pair::<String>("0.5,7.8", ','),
-            Some(("0.5".to_string(), "7.8".to_string()))
-        );
-    }
+  #[test]
+  fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(
+      parse_pair::<f64>("0.5x1.5", 'x'),
+      Some((0.5, 1.5))
+    );
+    assert_eq!(
+      parse_pair::<String>("0,5", ','),
+      Some(("0".to_string(), "5".to_string()))
+    );
+    assert_eq!(
+      parse_pair::<String>("0.5,7.8", ','),
+      Some(("0.5".to_string(), "7.8".to_string()))
+    );
+  }
 
-    #[test]
-    fn test_split_arg() {
-        assert_eq!(split_arg(""), None);
-        assert_eq!(
-            split_arg("10,"),
-            Some(vec!["10".to_string(), "".to_string()])
-        );
-        assert_eq!(
-            split_arg(",10"),
-            Some(vec!["".to_string(), "10".to_string()])
-        );
-        assert_eq!(
-            split_arg("10,20"),
-            Some(vec!["10".to_string(), "20".to_string()])
-        );
-        assert_eq!(
-            split_arg("10,20xy"),
-            Some(vec!["10".to_string(), "20xy".to_string()])
-        );
-        assert_eq!(
-            split_arg("0.5,x"),
-            Some(vec!["0.5".to_string(), "x".to_string()])
-        );
+  #[test]
+  fn test_split_arg() {
+    assert_eq!(split_arg(""), None);
+    assert_eq!(
+      split_arg("10,"),
+      Some(vec!["10".to_string(), "".to_string()])
+    );
+    assert_eq!(
+      split_arg(",10"),
+      Some(vec!["".to_string(), "10".to_string()])
+    );
+    assert_eq!(
+      split_arg("10,20"),
+      Some(vec!["10".to_string(), "20".to_string()])
+    );
+    assert_eq!(
+      split_arg("10,20xy"),
+      Some(vec!["10".to_string(), "20xy".to_string()])
+    );
+    assert_eq!(
+      split_arg("0.5,x"),
+      Some(vec!["0.5".to_string(), "x".to_string()])
+    );
 
-        assert_eq!(split_arg("0.5x"), None);
-        assert_eq!(split_arg("0.5x1.5"), None);
-    }
+    assert_eq!(split_arg("0.5x"), None);
+    assert_eq!(split_arg("0.5x1.5"), None);
+  }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,21 +1,21 @@
 #[cfg(test)]
 mod integration_test {
-    use std::process::Command;
+  use std::process::Command;
 
-    #[test]
-    fn test_run_with_valid_args() {
-        let output = Command::new("./target/debug/20min")
+  #[test]
+  fn test_run_with_valid_args() {
+    let output = Command::new("./target/debug/20min")
             .arg("0.01,0.01")  // work_t: 0.6 secs, rest_t: 0.6 secs
             .output().unwrap();
-        assert_eq!(String::from_utf8_lossy(&output.stderr), "");
-    }
+    assert_eq!(String::from_utf8_lossy(&output.stderr), "");
+  }
 
-    #[test]
-    fn test_run_with_invalid_work_t() {
-        let output = Command::new("./target/debug/20min")
-            .arg("foo")
-            .output()
-            .unwrap();
-        assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
-    }
+  #[test]
+  fn test_run_with_invalid_work_t() {
+    let output = Command::new("./target/debug/20min")
+      .arg("foo")
+      .output()
+      .unwrap();
+    assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
+  }
 }
