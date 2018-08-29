04e35c009dc6104ef1b9c7dc460696c9f6f969be
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2018-08-30 00:05:26 +0900

    Fix style

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,7 +8,8 @@ mod integration_test {
   fn test_run_with_invalid_args_with_comma() {
     let output = Command::new("./target/debug/20min")
       .arg("foo,bar")
-      .output().unwrap();
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
   }
 
@@ -16,7 +17,8 @@ mod integration_test {
   fn test_run_with_invalid_args_without_comma() {
     let output = Command::new("./target/debug/20min")
       .arg("foo")
-      .output().unwrap();
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
   }
 
@@ -26,12 +28,15 @@ mod integration_test {
 
     output = Command::new("./target/debug/20min")
       .arg("0.01,0.01")  // work: 0.6 secs, rest: 0.6 secs
-      .output().unwrap();
+      .output()
+      .unwrap();
     assert_eq!(String::from_utf8_lossy(&output.stderr), "");
 
     output = Command::new("./target/debug/20min")
-      .arg("0.01").arg("0.01")  // work: 0.6 secs, rest: 0.6 secs
-      .output().unwrap();
+      .arg("0.01")  // work: 0.6 secs, rest: 0.6 secs
+      .arg("0.01")
+      .output()
+      .unwrap();
     assert_eq!(String::from_utf8_lossy(&output.stderr), "");
   }
 
@@ -42,31 +47,44 @@ mod integration_test {
     let mut output;
 
     output = Command::new("./target/debug/20min")
-      .arg("--work").arg("foo")
-      .output().unwrap();
+      .arg("--work")
+      .arg("foo")
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
 
     output = Command::new("./target/debug/20min")
-      .arg("--rest").arg("foo")
-      .output().unwrap();
+      .arg("--rest")
+      .arg("foo")
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
 
     output = Command::new("./target/debug/20min")
-      .arg("--work").arg("3")
-      .arg("--rest").arg("foo")
-      .output().unwrap();
+      .arg("--work")
+      .arg("3")
+      .arg("--rest")
+      .arg("foo")
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
 
     output = Command::new("./target/debug/20min")
-      .arg("--work").arg("foo")
-      .arg("--rest").arg("3")
-      .output().unwrap();
+      .arg("--work")
+      .arg("foo")
+      .arg("--rest")
+      .arg("3")
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
 
     output = Command::new("./target/debug/20min")
-      .arg("--work").arg("foo")
-      .arg("--rest").arg("bar")
-      .output().unwrap();
+      .arg("--work")
+      .arg("foo")
+      .arg("--rest")
+      .arg("bar")
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
   }
 
@@ -74,7 +92,8 @@ mod integration_test {
   fn test_run_with_unknown_opts() {
     let output = Command::new("./target/debug/20min")
       .arg("--no-more-work")
-      .output().unwrap();
+      .output()
+      .unwrap();
     assert!(String::from_utf8_lossy(&output.stderr).contains("panicked"));
   }
 
@@ -83,15 +102,21 @@ mod integration_test {
     let mut output;
 
     output = Command::new("./target/debug/20min")
-      .arg("--work").arg("0.01")
-      .arg("--rest").arg("0.01")
-      .output().unwrap();
+      .arg("--work")
+      .arg("0.01")
+      .arg("--rest")
+      .arg("0.01")
+      .output()
+      .unwrap();
     assert_eq!(String::from_utf8_lossy(&output.stderr), "");
 
     output = Command::new("./target/debug/20min")
-      .arg("-w").arg("0.01")
-      .arg("-r").arg("0.01")
-      .output().unwrap();
+      .arg("-w")
+      .arg("0.01")
+      .arg("-r")
+      .arg("0.01")
+      .output()
+      .unwrap();
     assert_eq!(String::from_utf8_lossy(&output.stderr), "");
   }
 }
