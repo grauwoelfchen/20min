dc2899dc73581366c1ca13526e8a7cd6d41517a5
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2022-07-17 13:53:01 +0900

    Refactor integration tests

--- test/test.rs
+++ test/test.rs
@@ -1,2 +1,15 @@
 mod args;
 mod opts;
+
+use std::env;
+use std::process::{Command, Output};
+
+const DEFAULT_CARGO_TARGET_DIR: &str = "./target";
+
+fn run(args: &[&str]) -> Result<Output, std::io::Error> {
+    let target_dir = env::var("CARGO_TARGET_DIR")
+        .unwrap_or_else(|_| DEFAULT_CARGO_TARGET_DIR.to_string());
+    let program = format!("{}/debug/20min", target_dir);
+
+    Command::new(&program).args(args).output()
+}
